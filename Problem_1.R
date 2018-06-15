c138258b73db71b0d1b2caf538bba656f8f5be74
Nikoleta Deliyska <nikoleta7977@gmail.com>
2018-06-15 16:30:21 +0300

    Homework 4

--- Problem_1.R
+++ Problem_1.R
@@ -3,25 +3,64 @@
 library(dplyr)
 
 ## Read the data
-houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
+houseWork <-
+  read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
 str(houseWork)
 
 ## a)
+summary(houseWork)
+# 4252 females and 6764 males are observed in the data set
 
 ## b)
+hoursf <- houseWork[houseWork$sex == "f", "hours"]
+hoursm <- houseWork[houseWork$sex == "m", "hours"]
+muf <- mean(hoursf)
+mum <- mean(hoursm)
 
 ## c)
 
+male = ifelse (houseWork$sex == "m", TRUE, FALSE)
+female = ifelse (houseWork$sex == "f", TRUE, FALSE)
+houseWork <- within (houseWork,
+                     male <- male)
+houseWork <- within (houseWork,
+                     female <- female)
+
 ## d)
 
+model1 <- lm(hours ~ female, data = houseWork)
+summary(model1)
+
 ## e)
+#The intercept shows us the situation where female=TRUE=0, which means that
+# it shows us the estimated average hours of housework done by males per week. (32.81)
+# the coefficient beta1 = -14.46, shows that if we add another woman, the average
+# working hours per week will decrease.
 
 ## f)
+#H0 is beta1 >=0 and H1 beta1 <0. The null hypothesis states that the hours woman work
+# play a role in the average hours worked and the alternative hypothesis states the opposite.
+# Looking at the results from model1 we can reject the null hypothesis, as the models
+# shows that there is a negative relationship between the two variables.
 
 ## g)
+populationMean <- mean(houseWork$hours)
+testStatistic <- sqrt(11016) * (muf - populationMean) / 0.3186
+pt(testStatistic , df = 11014)
 
 ## h)
+# We can reject the null hypothesis since the probability of making a mistake
+# by rejecting it is almost 0.
 
 ## i)
 
+#The test assumes that the population follows a t-distribution, but
+# our expectations are that the population follows a normal distribution.
+
 ## j)
+
+model2 <- lm(hours ~ female + male, data = houseWork)
+summary(model2)
+
+# the beta2 coefficient shows NA because when male is true female is false and
+# this is expressed in the intercept coefficient
--- Problem_2.R
+++ Problem_2.R
@@ -1,13 +1,44 @@
 ## Homework 4, Problem 2
-crime <- read.delim('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv', stringsAsFactors = FALSE)
+crime <-
+  read.delim(
+    'https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv',
+    stringsAsFactors = FALSE
+  )
 str(crime)
 
 ## a)
 
+fit <- lm(C ~ HS, data = crime)
+summary(fit)
+#the intercept shows that the estimated crime rate in a country,
+# where no one has finished high school is around minus 50.
+# the beta1 coefficient = 1.48, shows that an increase of people who has graduated
+# high school with 1 percent is observed with an estimated increase of crime rate of 1.48%
 ## b)
+crime2 <- crime[,-1]
+pairs( ~ ., data = crime2)
 
 ## c)
 
+fit2 <- lm(C ~ HS + U, data = crime)
+summary(fit2)
+#We can explain the change in the sign is due to some kind of a relationship
+# between the two variables HS and U. There is multicolinearity, meaning one feature
+# variable can be explained by another. Because of this the estimates for the change
+# in the explained variable may not be accurate.
+
 ## d)
 
+# Even though the model shows that there is some kind of a negative relationship
+# between the two variabes we cannot conclude that there is causation.
+# (няма причинно-следствена връзка). Also there are other variables that
+#may influence the crime rate but are not included in our model.
+
 ## e)
+
+fit3 <- lm(C ~ HS + U + I, data = crime)
+summary(fit3)
+
+#We cannot reject the null hypothesis stating that beta1 and beta3 are zero with a
+#5% significance level since the p-value is more than 0.05
+# (0.4 for beta1 and 0.69 for beta3)
