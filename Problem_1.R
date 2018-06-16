f86216ec0f09d018a3128fba5450fbf5d7bd8ebb
Boyko Amarov <boyko.amarov@gmail.com>
2018-06-16 18:08:29 +0300

    Add review.

--- Problem_1.R
+++ Problem_1.R
@@ -19,8 +19,14 @@ mum <- mean(hoursm)
 
 ## c)
 
-male = ifelse (houseWork$sex == "m", TRUE, FALSE)
-female = ifelse (houseWork$sex == "f", TRUE, FALSE)
+## Easier:
+
+houseWork <- within(houseWork, {
+    female <- sex == 'f'
+})
+
+male <- ifelse (houseWork$sex == "m", TRUE, FALSE)
+female <- ifelse (houseWork$sex == "f", TRUE, FALSE)
 houseWork <- within (houseWork,
                      male <- male)
 houseWork <- within (houseWork,
@@ -32,18 +38,22 @@ model1 <- lm(hours ~ female, data = houseWork)
 summary(model1)
 
 ## e)
+## /score -1
 #The intercept shows us the situation where female=TRUE=0, which means that
 # it shows us the estimated average hours of housework done by males per week. (32.81)
 # the coefficient beta1 = -14.46, shows that if we add another woman, the average
 # working hours per week will decrease.
 
 ## f)
+## H0 states that women spend on average less time with housework than men.
+
 #H0 is beta1 >=0 and H1 beta1 <0. The null hypothesis states that the hours woman work
 # play a role in the average hours worked and the alternative hypothesis states the opposite.
 # Looking at the results from model1 we can reject the null hypothesis, as the models
 # shows that there is a negative relationship between the two variables.
 
 ## g)
+## Wrong test /score -2
 populationMean <- mean(houseWork$hours)
 testStatistic <- sqrt(11016) * (muf - populationMean) / 0.3186
 pt(testStatistic , df = 11014)
@@ -53,11 +63,12 @@ pt(testStatistic , df = 11014)
 # by rejecting it is almost 0.
 
 ## i)
-
+## /score -2
 #The test assumes that the population follows a t-distribution, but
 # our expectations are that the population follows a normal distribution.
 
 ## j)
+## Coefficient for male cannot be estimated due to multicollinearity.
 
 model2 <- lm(hours ~ female + male, data = houseWork)
 summary(model2)
--- Problem_2.R
+++ Problem_2.R
@@ -13,7 +13,7 @@ summary(fit)
 #the intercept shows that the estimated crime rate in a country,
 # where no one has finished high school is around minus 50.
 # the beta1 coefficient = 1.48, shows that an increase of people who has graduated
-# high school with 1 percent is observed with an estimated increase of crime rate of 1.48%
+# high school with 1 percent is observed with an estimated increase of crime rate of 1.48 (cases per 1000 persons)
 ## b)
 crime2 <- crime[,-1]
 pairs( ~ ., data = crime2)
@@ -22,13 +22,17 @@ pairs( ~ ., data = crime2)
 
 fit2 <- lm(C ~ HS + U, data = crime)
 summary(fit2)
+
+## /score -0.5 answer not specific enough
+
 #We can explain the change in the sign is due to some kind of a relationship
-# between the two variables HS and U. There is multicolinearity, meaning one feature
+# between the two variables HS and U (what kind of relationship???). There is multicolinearity, meaning one feature
 # variable can be explained by another. Because of this the estimates for the change
-# in the explained variable may not be accurate.
+                                        # in the explained variable may not be accurate.
+
 
 ## d)
-
+## /score -1 not specific enough!
 # Even though the model shows that there is some kind of a negative relationship
 # between the two variabes we cannot conclude that there is causation.
 # (няма причинно-следствена връзка). Also there are other variables that
@@ -42,3 +46,6 @@ summary(fit3)
 #We cannot reject the null hypothesis stating that beta1 and beta3 are zero with a
 #5% significance level since the p-value is more than 0.05
 # (0.4 for beta1 and 0.69 for beta3)
+
+## /score -5
+
